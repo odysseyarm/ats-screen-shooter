810505535f785d74a5938ed52165f31783bde97c
Drew Dunne <drewdunne88@gmail.com>
2025-09-02 15:00:56 -0500

    All the fixes

--- Assets/Input Actions/AppControls.cs
+++ Assets/Input Actions/AppControls.cs
@@ -781,6 +781,15 @@ public partial class @AppControls: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""ToggleResponsiveDistance"",
+                    ""type"": ""Button"",
+                    ""id"": ""a5b6c7d8-e9f0-1234-5678-90abcdef1234"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -1245,6 +1254,17 @@ public partial class @AppControls: IInputActionCollection2, IDisposable
                     ""action"": ""Escape"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""b9c8d0e1-f2a3-4567-89ab-cdef01234567"",
+                    ""path"": ""<Keyboard>/r"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard&Mouse"",
+                    ""action"": ""ToggleResponsiveDistance"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         },
@@ -1424,6 +1444,7 @@ public partial class @AppControls: IInputActionCollection2, IDisposable
         m_UI_ToggleUI = m_UI.FindAction("ToggleUI", throwIfNotFound: true);
         m_UI_SwitchScene = m_UI.FindAction("SwitchScene", throwIfNotFound: true);
         m_UI_Escape = m_UI.FindAction("Escape", throwIfNotFound: true);
+        m_UI_ToggleResponsiveDistance = m_UI.FindAction("ToggleResponsiveDistance", throwIfNotFound: true);
         // ExitModal
         m_ExitModal = asset.FindActionMap("ExitModal", throwIfNotFound: true);
         m_ExitModal_Exit = m_ExitModal.FindAction("Exit", throwIfNotFound: true);
@@ -1762,6 +1783,7 @@ public partial class @AppControls: IInputActionCollection2, IDisposable
     private readonly InputAction m_UI_ToggleUI;
     private readonly InputAction m_UI_SwitchScene;
     private readonly InputAction m_UI_Escape;
+    private readonly InputAction m_UI_ToggleResponsiveDistance;
     /// <summary>
     /// Provides access to input actions defined in input action map "UI".
     /// </summary>
@@ -1825,6 +1847,10 @@ public partial class @AppControls: IInputActionCollection2, IDisposable
         /// Provides access to the underlying input action "UI/Escape".
         /// </summary>
         public InputAction @Escape => m_Wrapper.m_UI_Escape;
+        /// <summary>
+        /// Provides access to the underlying input action "UI/ToggleResponsiveDistance".
+        /// </summary>
+        public InputAction @ToggleResponsiveDistance => m_Wrapper.m_UI_ToggleResponsiveDistance;
         /// <summary>
         /// Provides access to the underlying input action map instance.
         /// </summary>
@@ -1890,6 +1916,9 @@ public partial class @AppControls: IInputActionCollection2, IDisposable
             @Escape.started += instance.OnEscape;
             @Escape.performed += instance.OnEscape;
             @Escape.canceled += instance.OnEscape;
+            @ToggleResponsiveDistance.started += instance.OnToggleResponsiveDistance;
+            @ToggleResponsiveDistance.performed += instance.OnToggleResponsiveDistance;
+            @ToggleResponsiveDistance.canceled += instance.OnToggleResponsiveDistance;
         }
 
         /// <summary>
@@ -1940,6 +1969,9 @@ public partial class @AppControls: IInputActionCollection2, IDisposable
             @Escape.started -= instance.OnEscape;
             @Escape.performed -= instance.OnEscape;
             @Escape.canceled -= instance.OnEscape;
+            @ToggleResponsiveDistance.started -= instance.OnToggleResponsiveDistance;
+            @ToggleResponsiveDistance.performed -= instance.OnToggleResponsiveDistance;
+            @ToggleResponsiveDistance.canceled -= instance.OnToggleResponsiveDistance;
         }
 
         /// <summary>
@@ -2349,6 +2381,13 @@ public partial class @AppControls: IInputActionCollection2, IDisposable
         /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
         /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
         void OnEscape(InputAction.CallbackContext context);
+        /// <summary>
+        /// Method invoked when associated input action "ToggleResponsiveDistance" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
+        /// </summary>
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
+        void OnToggleResponsiveDistance(InputAction.CallbackContext context);
     }
     /// <summary>
     /// Interface to implement callback methods for all input action callbacks associated with input actions defined by "ExitModal" which allows adding and removing callbacks.
